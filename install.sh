af53895f19586fd7b6e7bde20e14ba94dd7b30e2
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2022-10-22 12:39:20 -0500

    Remove deploy folder in bin

--- install.sh
+++ install.sh
@@ -2,13 +2,12 @@
 
 # Check if the user is root
 if [ $(id -u) != "0" ]; then
-    echo "Error: You must be root to run this script, please use root to install"
-    exit 1
+    echo "Error: You must be root to run this script, please use root to install";
+    exit 1;
 fi
 
 folder=/usr/local/bin
 
-mkdir -p $folder/deploy;
-cp deploy.sh $folder/deploy/ -r;
-mv $folder/deploy/deploy.sh $folder/deploy/deploy;
-chmod u+x $folder/deploy/deploy;
+cp deploy.sh $folder/ -r;
+mv $folder/deploy.sh $folder/deploy;
+chmod u+x $folder/deploy;
